83de9ea78980bba69da12c393dac8be3e10d6091
Tabs16 <taybawasim@gmail.com>
2016-06-02 17:41:48 +0530

    new app.py

--- app.py
+++ app.py
@@ -1,9 +1,13 @@
-from flask import Flask
-app = Flask(_name_)
+from flask import Flask,render_template
+app = Flask(__name__)
 
 @app.route('/')
 def index():
-	return '<h1>Hello World</h1>'
+	return render_template('index.html')
+	
+@app.route('/home')
+def tayba():
+	return '<h1>Hello Tayba</h1>'
 
-if _name_ == '__main__':
+if __name__ == '__main__':
 	app.run(debug=True)
